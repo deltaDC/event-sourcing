c9ba84300f4ff5b1c37bbffe99a09b1cb3431733
deltadc <ducchinh.work@gmail.com>
2025-04-02 20:47:09 +0700

    buc minh vc

--- src/main/java/com/soa/eventsourcing/aggregate/OrderDetails.java
+++ src/main/java/com/soa/eventsourcing/aggregate/OrderDetails.java
@@ -4,13 +4,39 @@ package com.soa.eventsourcing.aggregate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+//@Data
+//@ToString
 public class OrderDetails {
 
     private String product;
     private int quantity;
     private double price;
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
